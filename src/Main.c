36c08c868c6317c8411b428abd051d49524a9712
Karol Sarnecki <Karol Sarnecki>
2016-06-06 21:28:43 +0100

    Poprawki w Main.c

--- src/Main.c
+++ src/Main.c
@@ -1,20 +1,12 @@
 
 #include "diods/diods_hl_api.h"
-#include "uart/uart_soft.h"
 #include "timer/timer_ll_api.h"
 
 int main() {
   
-  diodsInitSoft();
-  diodsInitHardware();
-  uartInitHardware();
+  runIrc();
+  runTimers();
   
-  timer_ll_api_init();
-
-  irc_ll_api_init();
-  irc_hl_api_init();
-
-  send("conf ok\r\n", 9);
   return 0;
 }
 

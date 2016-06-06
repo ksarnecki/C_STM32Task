f5f09bfa80a321e442d7d260449c7898de2cf033
Karol Sarnecki <Karol Sarnecki>
2016-06-06 22:30:51 +0100

    Poprawa bledow kompilacji

--- src/TimerSoftware.c
+++ src/TimerSoftware.c
@@ -1,14 +1,13 @@
 #include "TimerSoftware.h"
 
 void changeGreenFill (const int fill) {
-  changePwmState(3, FULL_FILL, fill/2, FULL_FILL -(percent/2));
+//  changePwmState(3, MAX_FILL, fill/2, MAX_FILL -( fill/2));
 }
 
 void changeRedFill (const int fill) {
-  changePwmState(4, FULL_FILL, fill/2, FULL_FILL -(percent/2));
+//  changePwmState(4, MAX_FILL, fill/2, MAX_FILL - (fill/2));
 }
 
 void changeBlueFill (const int fill) {
-  changePwmState(5, FULL_FILL, fill/2, FULL_FILL -(percent/2));
-
+//  changePwmState(5, MAX_FILL, fill/2, MAX_FILL - (fill/2));
 }
